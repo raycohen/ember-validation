c220d6818abec974ffde2e61c6382b3d2fbdd86e
Ray Cohen <raycohen@gmail.com>
2015-08-30 12:32:34 -0700

    introduce simple tests on isValid

---
+++ tests/unit/validation-test.js
@@ -0,0 +1,39 @@
+import { module, test } from 'qunit';
+import Validation from 'ember-validation/validation-pack';
+import Ember from 'ember';
+
+module('validation', 'Unit | Validation');
+
+const ageValidations = {
+  age: {
+    numericality: {
+      greaterThan: 13
+    }
+  }
+};
+
+test('validation that isValid', function(assert) {
+  let model = Ember.Object.create({
+    age: 30
+  });
+
+  let validation = Validation.create({
+    validatee: model,
+    validations: ageValidations
+  });
+
+  assert.equal(validation.get('isValid'), true, 'validation passes');
+});
+
+test('validation that is not isValid', function(assert) {
+  let model = Ember.Object.create({
+    age: 12
+  });
+
+  let validation = Validation.create({
+    validatee: model,
+    validations: ageValidations
+  });
+
+  assert.equal(validation.get('isValid'), false, 'validation failed');
+});
